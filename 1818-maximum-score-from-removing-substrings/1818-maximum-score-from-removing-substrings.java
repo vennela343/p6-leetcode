654bf273e2cf4daeae151ae0d8ba0411827899e6
vennela343 <vennelamaisan@gmail.com>
2025-07-23 20:12:17 +0530

    Time: 207 ms (19.18%), Space: 47.6 MB (15.75%) - LeetHub

---
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int maximumGain(String s, int x, int y) {
+        int totalScore = 0;
+        String highPriorityPair = x > y ? "ab" : "ba";
+        String lowPriorityPair = highPriorityPair.equals("ab") ? "ba" : "ab";
+        String stringAfterFirstPass = removeSubstring(s, highPriorityPair);
+        int removedPairsCount =
+            (s.length() - stringAfterFirstPass.length()) / 2;
+        totalScore += removedPairsCount * Math.max(x, y);
+        String stringAfterSecondPass = removeSubstring(
+            stringAfterFirstPass,
+            lowPriorityPair
+        );
+        removedPairsCount = (stringAfterFirstPass.length() -
+            stringAfterSecondPass.length()) /
+        2;
+        totalScore += removedPairsCount * Math.min(x, y);
+
+        return totalScore;
+    }
+
+    private String removeSubstring(String input, String targetPair) {
+        Stack<Character> charStack = new Stack<>();
+        for (int i = 0; i < input.length(); i++) {
+            char currentChar = input.charAt(i);
+            if (
+                currentChar == targetPair.charAt(1) &&
+                !charStack.isEmpty() &&
+                charStack.peek() == targetPair.charAt(0)
+            ) {
+                charStack.pop(); // Remove the matching character from the stack
+            } else {
+                charStack.push(currentChar);
+            }
+        }
+        StringBuilder remainingChars = new StringBuilder();
+        while (!charStack.isEmpty()) {
+            remainingChars.append(charStack.pop());
+        }
+        return remainingChars.reverse().toString();
+    }
+}
+   

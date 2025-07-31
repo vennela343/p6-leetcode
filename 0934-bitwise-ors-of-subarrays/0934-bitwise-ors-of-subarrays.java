341f308d16124f67f8f0dc6343164dad00dd5dd7
vennela343 <vennelamaisan@gmail.com>
2025-07-31 20:47:56 +0530

    Time: 362 ms (43.44%), Space: 73.5 MB (47.54%) - LeetHub

---
+++ 0934-bitwise-ors-of-subarrays/0934-bitwise-ors-of-subarrays.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int subarrayBitwiseORs(int[] arr) {
+        Set<Integer> ans = new HashSet();
+        Set<Integer> cur = new HashSet();
+        cur.add(0);
+        for (int x: arr) {
+            Set<Integer> cur2 = new HashSet();
+            for (int y: cur)
+                cur2.add(x | y);
+            cur2.add(x);
+            cur = cur2;
+            ans.addAll(cur);
+        }
+
+        return ans.size();
+    }
+}
+    

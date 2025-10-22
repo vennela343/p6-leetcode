bb2c0220bdda1ee972694a8bf5d3c76587abd7e8
vennela <vennelamaisan@gmail.com>
2025-10-22 14:37:16 +0530

    Time: 411 ms (50.37%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 select max(num) as num
-from MyNumbers where num in(select num 
+from (select num 
 from MyNumbers
 group by num
-having count(num)=1)
+having count(num)=1) as n;
 
